b6137a462aaa08b5a56b74b1b144d9a05a8b6cf9
Ameh Christopher <akrine2christophine@gmail.com>
2023-05-05 14:02:29 +0700

    project setup

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+
+const setting = 'images/settings.png';
+const vouchers = 'images/voucher.png';
+const wallets = 'images/wallet.png';
+const crop = 'images/crop.png';
+const padlock = 'images/lock.png';
+const commentIcon = 'images/comment-alt.png';
+const shareIcon = 'images/share.png';
