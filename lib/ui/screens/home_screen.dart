fded99ee60f04661b84b12f2c065c4905a656cc5
Chukwuebuka Onah <chukwuebuka@kobo360.com>
2023-05-05 14:39:43 +0100

    refactor codebase

---
+++ lib/ui/screens/home_screen.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:dribble_design/constant.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key,});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  bool isCurrentIndex = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          // Expanded(child: Container(color: Colors.red,)),
+          const SizedBox(
+            height: 60,
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 20, right: 10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(6.0),
+                            borderSide: const BorderSide(color: Colors.grey)),
+                        prefixIcon: const Icon(
+                          Icons.search,
+                          size: 30,
+                        ),
+                        hintText: 'Search'),
+                  ),
+                ),
+                Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.camera_alt),
+                        onPressed: () {
+                          // add your code here
+                        },
+                      ),
+                      // Image.asset(padlock, height: 60, width: 20,),
+                      Positioned(
+                          left: 24,
+                          bottom: 25,
+                          child: Container(
+                            height: 20,
+                            width: 20,
+                            color: Colors.pinkAccent,
+                            child: const Center(child: Text('1', style: TextStyle(color: Colors.white),)),
+                          )
+                      )
+                    ]
+                ),
+                Stack(
+                    alignment: Alignment.topCenter,
+                    children: [
+                      IconButton(
+                        icon: const Icon(Icons.camera_alt),
+                        onPressed: () {
+                          // add your code here
+                        },
+                      ),
+                      // Image.asset(padlock, height: 60, width: 20,),
+                      Positioned(
+                          left: 24,
+                          bottom: 25,
+                          child: Container(
+                            height: 20,
+                            width: 20,
+                            color: Colors.pinkAccent,
+                            child: const Center(child: Text('9+', style: TextStyle(color: Colors.white),)),
+                          )
+                      )
+                    ]
+                ),
+
+              ],
+            ),
+          ),
+          Expanded(
+              child: Container(
+                color: Colors.blue,
+              )),
+          Expanded(
+              child: Container(
+                color: Colors.yellow,
+              ))
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.black,
+        selectedLabelStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+        selectedIconTheme: const IconThemeData(color: Color(0xFF4DAB95)),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_filled,
+              ),
+              label: 'Home'),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage(vouchers)), label: 'Voucher'),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage(wallets)), label: 'Wallet'),
+          BottomNavigationBarItem(
+              icon: ImageIcon(AssetImage(setting)), label: 'Settings')
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
